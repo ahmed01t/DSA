7b260230738d2215b521891174648f047247181d
Ahmed Tanveer <ahmedtanveer0326@gmail.com>
2025-07-03 12:20:58 -0700

    add

--- arrays_/reverse_arrray.cpp
+++ arrays_/reverse_arrray.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int arr[3]={2,3,4};
-    for (int i=0;i<3;i++){
-        cout<<arr[i];
-    }
+int arr[5]={1,2,3,4,5};
+int size=sizeof(arr)/sizeof(arr[0]);
+int start=0;
+int end=size-1;
+while (start<end){
+    int temp=arr[start];
+    arr[start]=arr[end];
+    arr[end]=temp;
+    start++;
+    end--;
+}
+for (int i=0;i<size;i++){
+    cout<<arr[i];
+}
 }
