7e4e5cd967d929c5db4578fd367934bda42e29db
ahmed01t <ahmedtanveer0326@gmail.com>
2026-04-20 02:46:55 -0700

    update

---
+++ LINKLIST/class.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+// Node class
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+// Linked List class
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        head = NULL;
+    }
+
+    // Insert at end
+    void insert(int val) {
+        Node* newNode = new Node(val);
+
+        if (head == NULL) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    // Display list
+    void display() {
+        Node* temp = head;
+
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    // Delete a value
+    void deleteValue(int val) {
+        if (head == NULL) return;
+
+        // If head is to be deleted
+        if (head->data == val) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != NULL && temp->next->data != val) {
+            temp = temp->next;
+        }
+
+        if (temp->next == NULL) return; // value not found
+
+        Node* nodeToDelete = temp->next;
+        temp->next = temp->next->next;
+        delete nodeToDelete;
+    }
+};
+
+// Main function
+int main() {
+    LinkedList list;
+
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+
+    cout << "Linked List: ";
+    list.display();
+
+    list.deleteValue(20);
+
+    cout << "After Deletion: ";
+    list.display();
+
+    return 0;
+}
